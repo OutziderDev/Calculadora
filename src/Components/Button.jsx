9454ee6f573139d9892743d6bfc4a7434a2b4511
Luis <luisbarrios0110@gmail.com>
2025-06-22 16:59:28 -0500

    chore: reset project for Astro rebuild

--- src/Components/Button.jsx
+++
@@ -1,7 +0,0 @@
-const Button = (props) => {
-    return(
-      <button className='bg-slate-200 rounded hover:bg-slate-400 active:translate-y-0.5'>{props.valor}</button>
-    )
-}
-
-export default Button;
--- src/Components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () =>{
-    return(
-        <div className="w-full fixed top-0 h-16 bg-cyan-700">
-        
-        </div>
-        
-    )
-}
-export default Navbar;
